ca500ff644ae2dc1547fe12ca5c69ba836a62cb9
Niyas <niyasjahangeer772@gmail.com>
2025-12-01 23:49:28 +0530

    fix: add proper declaration files for jsx modules

---
+++ src/pages/admin/index.d.ts
@@ -0,0 +1,5 @@
+declare module './index' {
+  import { ComponentType } from 'react';
+  const component: ComponentType<any>;
+  export default component;
+}
---
+++ src/pages/admin/login.d.ts
@@ -0,0 +1,5 @@
+declare module './login' {
+  import { ComponentType } from 'react';
+  const component: ComponentType<any>;
+  export default component;
+}
